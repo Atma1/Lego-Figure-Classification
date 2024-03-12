614abbe42c6902f8d591b1eabf563a338761375d
atmasusanto08@gmail.com <Atma1>
2024-03-12 10:41:25 +0800

    init

---
+++ zenml/steps/data_loader.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+from zenml import step
+from fastai.vision.all import DataBlock, ImageBlock, CategoryBlock, RandomSplitter, ColReader, Resize, IntToFloatTensor, Normalize
+from typing_extensions import Annotated
+from torchvision.transforms import CenterCrop
+
+@step
+def create_dataloader(data: pd.DataFrame, random_state: int=42) ->Annotated[DataBlock, "Dataloader"]:
+    mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+    batch_tfms = [IntToFloatTensor(), Normalize(mean=mean, std=std)]
+    dl = DataBlock(blocks=(ImageBlock, CategoryBlock),
+                   splitter=RandomSplitter(seed=random_state),
+                   get_x=ColReader('path', pref=str(dir) + os.path.sep),
+                   get_y=ColReader('class_label'),
+                   item_tfms=[Resize(256), CenterCrop(256)],
+                   batch_tfms=batch_tfms)
+    dls = dl.dataloaders(data)
+    return dls
---
+++ zenml/steps/data_transform.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+from zenml import step
+from typing_extensions import Annotated
+
+ROOT_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+@step
+def data_transform(df_dir: str) -> Annotated[pd.DataFrame, "transformed_data"]:
+    metadata_csv_dir = os.path.join(ROOT_DIR , f"{df_dir}/metadata.csv")
+    index_csv_dir = os.path.join(ROOT_DIR, f"{df_dir}/index.csv")
+    index_label_series = pd.read_csv(metadata_csv_dir)["minifigure_name"]
+    index_csv_df = pd.read_csv(index_csv_dir)
+    index_csv_df["class_id"] = index_csv_df["class_id"].map(lambda value: index_label_series[int(value)-1])
+    transformed_data = index_csv_df.rename({"path": "path", "class_id": "class_label"})
+    return transformed_data
+
+
+
