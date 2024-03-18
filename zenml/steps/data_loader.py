1ed287c5488af7efa622fe133a21e415c8d4b5a0
atmasusanto08@gmail.com <Atma1>
2024-03-18 10:20:30 +0800

    added validation percentage argument

--- zenml/steps/data_loader.py
+++ zenml/steps/data_loader.py
@@ -6,11 +6,11 @@ from typing_extensions import Annotated
 from torchvision.transforms import CenterCrop
 
 @step
-def create_dataloader(data: pd.DataFrame, random_state: int=42) ->Annotated[DataBlock, "Dataloader"]:
+def create_dataloader(data: pd.DataFrame, random_state: int=42, valid_pct: float=0.2) ->Annotated[DataBlock, "Dataloader"]:
     mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
     batch_tfms = [IntToFloatTensor(), Normalize(mean=mean, std=std)]
     dl = DataBlock(blocks=(ImageBlock, CategoryBlock),
-                   splitter=RandomSplitter(seed=random_state),
+                   splitter=RandomSplitter(seed=random_state, valid_pct=valid_pct),
                    get_x=ColReader('path'),
                    get_y=ColReader('class_label'),
                    item_tfms=[Resize(256), CenterCrop(256)],
