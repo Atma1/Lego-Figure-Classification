5958067d56cd5316efb2034ee06765d5cb62cf47
atmasusanto08@gmail.com <Atma1>
2024-03-19 14:35:18 +0800

    changed indentation

--- zenml/steps/data_loader.py
+++ zenml/steps/data_loader.py
@@ -6,7 +6,8 @@ from typing_extensions import Annotated
 from torchvision.transforms import CenterCrop
 
 @step
-def create_dataloader(data: pd.DataFrame, random_state: int=42, valid_pct: float=0.2) ->Annotated[DataLoaders, "Dataloader"]:
+def create_dataloader(data: pd.DataFrame, random_state: int=42, valid_pct: float=0.2
+                      ) ->Annotated[DataLoaders, "Dataloader"]:
     mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
     batch_tfms = [IntToFloatTensor(), Normalize(mean=mean, std=std)]
     dl = DataBlock(blocks=(ImageBlock, CategoryBlock),
