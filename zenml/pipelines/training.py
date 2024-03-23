d742b9b22e8110b069ab5cec9e942f24191aca84
Atma 1 <atmasusanto08@gmail.com>
2024-03-23 12:12:01 +0800

    log accuracy artifcact

--- zenml/pipelines/training.py
+++ zenml/pipelines/training.py
@@ -1,11 +1,18 @@
-from zenml import pipeline
+from zenml import pipeline, log_artifact_metadata
 from pipelines import data_engineering
 from steps import train
 
 
 @pipeline
 def training_model(lr: float):
+
     dataloaders = data_engineering()
     trained_model = train(dls=dataloaders, lr=lr)
+    
     metrics = trained_model.recorder.metrics
     accuracy = metrics[0].value.item()
+
+    log_artifact_metadata(
+        metadata={"accuracy": float(accuracy)},
+        artifact_name="resnet18",
+    )
