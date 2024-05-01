1830153cc1448fcd6e0d2726e8b9e26f16770f07
Atma1 <Atma1>
2024-05-01 15:45:14 +0800

    return none

--- zenml/steps/model_promoter.py
+++ zenml/steps/model_promoter.py
@@ -6,7 +6,7 @@ logger = get_logger(__name__)
 
 
 @step
-def model_promoter(accuracy: float, stage: str = "production"):
+def model_promoter(accuracy: float, stage: str = "production") -> None:
 
     if accuracy < 0.8:
         logger.info(
