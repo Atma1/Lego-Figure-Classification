ef398f6f847ddb4860ef955235856fc8160bf36c
Atma1 <atmasusanto08@gmail.com>
2024-05-24 12:13:38 +0800

    renamed column name for clearity

--- zenml/steps/data_transform.py
+++ zenml/steps/data_transform.py
@@ -22,5 +22,5 @@ def data_transform(df_dir: str) -> Annotated[pd.DataFrame, "transformed_data"]:
     index_csv_df = pd.read_csv(index_csv_dir)
     index_csv_df["path"] = index_csv_df["path"].map(lambda value: f"{ROOT_DIR}/{df_dir}/{value}")
     index_csv_df["class_id"] = index_csv_df["class_id"].map(lambda value: index_label_series[int(value)-1])
-    index_csv_df.rename(columns={"path": "path", "class_id": "class_label"}, inplace=True)
+    index_csv_df.rename(columns={"path": "img_path", "class_id": "class_label"}, inplace=True)
     return index_csv_df
