695e137135c6faf7d7a7b5919d6af20b7b36748b
Atma1 <atmasusanto08@gmail.com>
2024-06-05 20:38:55 +0800

    added mlflow tracking and model logging

--- zenml/steps/train.py
+++ zenml/steps/train.py
@@ -1,4 +1,5 @@
-from zenml import step, ArtifactConfig
+import mlflow.fastai
+from zenml import step, ArtifactConfig, log_artifact_metadata
 from fastai.vision.all import *
 from typing_extensions import Annotated
 from zenml.logger import get_logger
@@ -18,17 +19,22 @@ def data_loader(data: pd.DataFrame, random_state: int=42, valid_pct: float=0.2
                    get_y=ColReader('class_label'),
                    item_tfms=[CropPad(256), Resize(256)],
                    batch_tfms=batch_tfms)
-    dls = dl.dataloaders(data, verbose=True)
+    dls = dl.dataloaders(data, verbose=False)
     return dls
 
-@step
+@step(experiment_tracker="mlflow_experiment_tracker")
 def train(transformed_data: pd.DataFrame, lr: float=1e-2
-                ) -> Annotated[Learner, ArtifactConfig(name="resnet18", is_model_artifact=True)]:
+                ) -> Tuple[Annotated[Learner, ArtifactConfig(name="resnet18", is_model_artifact=True)],
+                           Annotated[str, "run_id"]]:
     """
     Model promoter step
     """
     dls = data_loader(transformed_data)
     learn = vision_learner(dls, resnet18, metrics=accuracy, lr=lr)
+    mlflow.fastai.autolog()
     logger.info("Training model.....")
-    learn.fine_tune(10)
-    return learn
+    with mlflow.start_run(nested=True, run_name="train") as run:
+        learn.fit(1)
+    run_id = run.info.run_id
+
+    return (learn, run_id)
