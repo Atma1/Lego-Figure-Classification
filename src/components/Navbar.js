0276851f9773934514791ce2d5e45dad151cb987
Atma1 <atmasusanto08@gmail.com>
2024-07-01 17:53:17 +0800

    added style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <nav id="nav-bar">
-            <h1 id="web-title"><i>Lego Figure Classification</i></h1>
+            <h1 id="web-title" className="text-2xl py-2 font-bold"><i>Lego Figure Classification</i></h1>
         </nav>
     );
 }
