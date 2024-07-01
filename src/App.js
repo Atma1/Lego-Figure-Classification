75dc10db4644a079541b1406a2c21b6c84a9bf4a
Atma1 <atmasusanto08@gmail.com>
2024-07-01 17:52:55 +0800

    moved some function outside App function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './styles.css';
+import './index.css';
 import Body from './components/Body';
 import Navbar from './components/Navbar';
 import React from 'react';
 
 const endpoint = "http://localhost:5000/predict"
+
+const getPrediction = async (image) => {
+  let formData = new FormData();
+  formData.append('image', image)
+
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    body: formData,
+});
+  return await response.json();
+}
 
 export default function App() {
   const [file, setFile] = React.useState();
@@ -13,23 +24,10 @@ export default function App() {
     setFile(e.target.files[0]);
   }
 
-  const getPrediction = async (image) => {
-    let formData = new FormData();
-    formData.append('image', image)
-
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      body: formData,
-  });
-    console.log(response);
-    return await response.json();
-  }
-
   const handleUploadClick = async (image) => {
     if (file) {
       try {
         const res = await getPrediction(image);
-        console.log(res);
         setPrediction(res);
       } catch (error) {
         console.error(error);
