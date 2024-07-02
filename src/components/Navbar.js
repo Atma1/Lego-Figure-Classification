e49ef13e2f60258070b89e956be8d81653a88e22
Atma1 <atmasusanto08@gmail.com>
2024-07-02 15:40:51 +0800

    added react logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
+import logo from "../logo512.png";
+
 export default function Navbar() {
     return (
         <nav id="nav-bar">
+            <img src={logo} className="h-8 w-8 ml-2 mt-1" alt="logo"/>
             <h1 id="web-title" className="text-2xl py-2 font-bold"><i>Lego Figure Classification</i></h1>
         </nav>
     );
