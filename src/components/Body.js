3effedc7a2f7d9d947266a55c8974bff60e69a92
Atma1 <atmasusanto08@gmail.com>
2024-07-02 15:40:39 +0800

    added shadow and adjusted margin for submit button

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,14 +5,14 @@ export default function Body({file, handleFileInputChange, handleUploadClick, pr
             <h3 class="model-output" id="modelOutput">Prediction: {prediction.class}</h3>
             <h3 class="model-output-confidence" id="modelOutputConfidencess">Confidence: {prediction.confidence}</h3>
         </div>
-        <div class="image-preview" id="imagePreview">
+        <div class="image-preview shadow-xl" id="imagePreview">
             {file && <img src={URL.createObjectURL(file)} alt='' class="image-preview-image"/>}
             {!file && <span class="image-preview-default-text" id="image-preview-text">No Image Selected</span>}
         </div>
         <div id="button-and-input-container">
             <label for="imagePreview" id="input-label">Choose Image To Classifiy</label>
             <input type="file" name="imagePreview" id="image-input" accept=".jpg, .jpeg" onChange={handleFileInputChange}/>
-            <button type="submit" class="image-upload-button bg-white hover:bg-gray-200" id="imageUploadButton" onClick={() => handleUploadClick(file)}>Get Prediction</button>
+            <button type="submit" class="image-upload-button bg-white hover:bg-gray-200 mt-2" id="imageUploadButton" onClick={() => handleUploadClick(file)}>Get Prediction</button>
         </div>
     </>
     );
